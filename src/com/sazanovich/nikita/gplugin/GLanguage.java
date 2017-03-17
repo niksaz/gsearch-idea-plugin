ddfa414f40c784110b04fba9ca8a9b1e3da6fc8d
niksaz <nikitasazanovich@gmail.com>
2017-03-18 01:31:08 +0300

    initialized repo

---
+++ src/com/sazanovich/nikita/gplugin/GLanguage.java
@@ -0,0 +1,12 @@
+package com.sazanovich.nikita.gplugin;
+
+import com.intellij.lang.Language;
+
+public class GLanguage extends Language {
+
+    public static final GLanguage INSTANCE = new GLanguage();
+
+    private GLanguage() {
+        super("Google Search Language");
+    }
+}
